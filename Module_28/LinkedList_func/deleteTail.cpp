4a7093c97b1086341ebc461e4a6c6c09c4577c95
Rishabh Jha <rishabhjha012043@gmail.com>
2025-07-25 07:17:17 +0530

    linkelist functions added

---
+++ Module_28/LinkedList_func/deleteTail.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val) : val(val), next(NULL) {}
+};
+class linkedlist
+{
+public:
+    Node *head, *tail;
+    int size;
+    linkedlist()
+    {
+        head = tail = NULL;
+        size = 0;
+    }
+    void display()
+    {
+        Node *temp = head;
+        while (temp != 0)
+        {
+            cout << temp->val << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+        return;
+    }
+    void addAtHead(int val)
+    {
+        Node *temp = new Node(val);
+        if (size != 0)
+        {
+            temp->next = head;
+            head = temp;
+        }
+        else
+            head = tail = temp;
+        size++;
+        return;
+    }
+    void addAtTail(int val)
+    {
+        Node *temp = new Node(val);
+        if (size != 0)
+        {
+            tail->next = temp;
+            tail = temp;
+        }
+        else
+            head = tail = temp;
+        size++;
+        return;
+    }
+    void insert(int idx, int val)
+    {
+        if (idx < 0 || idx > size)
+            cout << "error, invalid argument";
+        else if (idx == 0)
+            addAtHead(val);
+        else if (idx == size)
+            addAtTail(val);
+        else
+        {
+            Node *temp = head;
+            Node *temp2 = new Node(val);
+            for (int i = 1; i < idx; i++)
+                temp = temp->next;
+            temp2->next = temp->next;
+            temp->next = temp2;
+        }
+        size++;
+        return;
+    }
+
+    void deleteHead() { head = head->next, size--; }
+    void deleteTail()
+    {
+        Node *temp = head;
+        while (temp->next != tail)
+            temp = temp->next;
+        temp->next = NULL;
+        size--;
+        return;
+    }
+};
+int main()
+{
+    linkedlist ll;
+    ll.addAtHead(30);
+    ll.addAtHead(10);
+    ll.display();
+    ll.insert(1, 20);
+    ll.display();
+    ll.addAtTail(12);
+    ll.display();
+    ll.deleteTail();
+    cout << "Linked list after deleting the tail is : ";
+    ll.display();
+
+    return 0;
+}
